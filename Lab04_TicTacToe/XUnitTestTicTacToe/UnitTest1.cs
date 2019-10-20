a6f2422dca2dacfbb712207d00e78a599978ee91
darrikpuetz <darrikpuetz@gmail.com>
2019-10-20 10:57:00 -0700

    Created winner and Tie test

--- Lab04_TicTacToe/XUnitTestTicTacToe/UnitTest1.cs
+++ Lab04_TicTacToe/XUnitTestTicTacToe/UnitTest1.cs
@@ -23,5 +23,42 @@ namespace XUnitTestTicTacToe
 
 
         }
+        [Fact]
+        public void CheckWinner()
+        {
+            Player chris = new Player();
+            chris.Marker = "X";
+            Player enrique = new Player();
+            chris.Marker = "O";
+
+            Game testGame = new Game(chris, enrique);
+            testGame.Board.GameBoard[0, 2] = "X";
+            testGame.Board.GameBoard[1, 2] = "O";
+            testGame.Board.GameBoard[2, 2] = "X";
+            Assert.False(testGame.CheckForWinner(testGame.Board));
+
+
+        }
+        [Fact]
+        public void CheckTie()
+        {
+            //Need to find a better way to check the tie.
+            Player chris = new Player();
+            chris.Marker = "X";
+            Player enrique = new Player();
+            chris.Marker = "O";
+
+            Game testGame = new Game(chris, enrique);
+            testGame.Board.GameBoard[0, 0] = "X";
+            testGame.Board.GameBoard[1, 0] = "O";
+            testGame.Board.GameBoard[0, 1] = "X";
+            testGame.Board.GameBoard[1, 1] = "O";
+            testGame.Board.GameBoard[0, 2] = "O";
+            testGame.Board.GameBoard[1, 2] = "X";
+            testGame.Board.GameBoard[2, 2] = "X";
+            Assert.False(testGame.CheckForWinner(testGame.Board));
+
+
+        }
     }
 }

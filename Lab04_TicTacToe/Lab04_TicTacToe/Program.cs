e02bdf65b5412916e4ef2a19b365cbbe6fbec1c9
darrikpuetz <darrikpuetz@gmail.com>
2019-10-25 10:25:05 -0700

    Checked next player

--- Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -26,6 +26,7 @@ namespace Lab04_TicTacToe
 
             Game game = new Game(darrik, kyungrae);
             game.Play();
+
         }
         // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
         // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
